4ea24dbeb0ee6d51df42a054f3ce83537e5993f6
gabriellocp <gabriel_campista@hotmail.com>
2024-01-16 14:45:43 -0300

    feat: Using express.json() to get request body

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ const routes = require('./routes')
 
 const app = express()
 
+app.use(express.json())
 app.use(routes)
 app.listen(3000, () => console.log('Server listennig at http://localhost:3000'))
