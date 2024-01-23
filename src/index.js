5f1b3d1f53a3b43ee79c19f2b5f3470306fcec22
gabriellocp <gabriel_campista@hotmail.com>
2024-01-23 17:46:12 -0300

    feat: Added Middleware to deal with CORS requests

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ const routes = require('./routes')
 const app = express()
 
 app.use(express.json())
+// CORS Middleware
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001')
+  next()
+})
 app.use(routes)
 app.listen(3000, () => console.log('Server listennig at http://localhost:3000'))
