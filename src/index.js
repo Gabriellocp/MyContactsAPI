f23c07fd9919fa37888e8721d8f9d42b21794599
gabriellocp <gabriel_campista@hotmail.com>
2024-01-23 17:48:19 -0300

    refactor: Moved CORS Middleware to its own file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 const express = require('express')
+const cors = require('./app/middlewares/cors')
 const routes = require('./routes')
 
 const app = express()
 
 app.use(express.json())
 // CORS Middleware
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001')
-  next()
-})
+app.use()
 app.use(routes)
 app.listen(3000, () => console.log('Server listennig at http://localhost:3000'))
