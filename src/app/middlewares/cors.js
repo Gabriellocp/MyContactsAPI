49f38e53451b4dac310cb16cbdc4a91f4f7aec3c
gabriellocp <gabriel_campista@hotmail.com>
2024-01-24 14:54:52 -0300

    feat: Adding CORS configuration

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,4 +1,7 @@
 module.exports = (_, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001')
+  res.setHeader('Access-Control-Allow-Methods', '*')
+  // Cache current OPTIONS for preflight requests for X seconds
+  res.setHeader('Access-Control-Max-Age', '15')
   next()
 }
